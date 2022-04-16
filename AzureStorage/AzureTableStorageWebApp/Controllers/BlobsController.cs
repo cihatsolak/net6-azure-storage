619f7966c3d8ad72394f88e4c0746c8026a5271f
Cihat Solak <cihatsolak@hotmail.com>
2022-04-16 17:38:00 +0300

    Creating delete and download methods from blob storage

--- AzureStorage/AzureTableStorageWebApp/Controllers/BlobsController.cs
+++ AzureStorage/AzureTableStorageWebApp/Controllers/BlobsController.cs
@@ -30,5 +30,19 @@
             await _blobStorage.UploadAsync(formfile.OpenReadStream(), fileName, EContainerName.images);
             return RedirectToAction(nameof(Index));
         }
+
+        [HttpGet]
+        public async Task<IActionResult> Download(string fileName)
+        {
+            var stream = await _blobStorage.DownloadAsync(fileName, EContainerName.images);
+            return File(stream, "application/octet-stream", fileName);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(string fileName)
+        {
+            await _blobStorage.DeleteAsync(fileName, EContainerName.images);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
