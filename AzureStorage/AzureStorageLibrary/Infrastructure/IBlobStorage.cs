ae0e61a6ddf269a77b4705b51cf58f1c50805da6
Cihat Solak <cihatsolak@hotmail.com>
2022-04-16 15:31:58 +0300

    Creating an IBlobStorage interface

---
+++ AzureStorage/AzureStorageLibrary/Infrastructure/IBlobStorage.cs
@@ -0,0 +1,22 @@
+﻿namespace AzureStorageLibrary.Infrastructure
+{
+    public enum EContainerName
+    {
+        Image,
+        Pdf,
+        Office,
+        Video,
+        Log
+    }
+
+    public interface IBlobStorage
+    {
+        string BlobUrl { get; set; }
+        List<string> GetNames(EContainerName eContainerName);
+        Task UploadAsync(Stream fileStrem, string fileName, EContainerName eContainerName);
+        Task<Stream> DownloadAsync(string fileName, EContainerName eContainerName);
+        Task DeleteAsync(string fileName);
+        Task AddLogAsync(string text, string fileName);
+        Task<List<string>> GetLogsAsync(string fileName);
+    }
+}
